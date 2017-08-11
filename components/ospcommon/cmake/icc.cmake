6fb1cf0f3d388435e82567a86f44335298f5ef1a
Joao Barbosa <barbosa.garcia@gmail.com>
2017-08-11 08:48:46 -0500

    Minor compile fixes for mac

--- components/ospcommon/cmake/icc.cmake
+++ components/ospcommon/cmake/icc.cmake
@@ -32,8 +32,9 @@ IF (APPLE)
   SET (CMAKE_SHARED_LINKER_FLAGS "-dynamiclib ${CMAKE_SHARED_LINKER_FLAGS_INIT}")
   SET(CMAKE_CXX_FLAGS "-mmacosx-version-min=10.7 ${CMAKE_CXX_FLAGS}") # we only use MacOSX 10.7 features
   SET(CMAKE_CXX_FLAGS "-stdlib=libc++ ${CMAKE_CXX_FLAGS}") # link against C++11 stdlib
+ELSE()
+  SET(CMAKE_SHARED_LINKER_FLAGS "-Wl,--exclude-libs=ALL ${CMAKE_SHARED_LINKER_FLAGS}")
 ENDIF()
 
 # enable -static-intel and avoid to export ICC specific symbols from OSPRay
 SET(CMAKE_CXX_FLAGS "-static-intel ${CMAKE_CXX_FLAGS}")
-SET(CMAKE_SHARED_LINKER_FLAGS "-Wl,--exclude-libs=ALL ${CMAKE_SHARED_LINKER_FLAGS}")
